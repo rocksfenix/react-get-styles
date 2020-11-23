05abe072eb11bec977594a6effb241747c88df68
Gerardo Gallegos <puxerop@gmail.com>
2020-11-23 09:50:55 -0600

    adding list vendor prefixes

---
+++ src/vendor-properties.ts
@@ -0,0 +1,43 @@
+export const vendorSpecificProperties = [
+  'animation',
+  'animationDelay',
+  'animationDirection',
+  'animationDuration',
+  'animationFillMode',
+  'animationIterationCount',
+  'animationName',
+  'animationPlayState',
+  'animationTimingFunction',
+  'appearance',
+  'backfaceVisibility',
+  'backgroundClip',
+  'borderImage',
+  'borderImageSlice',
+  'boxSizing',
+  'boxShadow',
+  'contentColumns',
+  'transform',
+  'transformOrigin',
+  'transformStyle',
+  'transition',
+  'transitionDelay',
+  'transitionDuration',
+  'transitionProperty',
+  'transitionTimingFunction',
+  'perspective',
+  'perspectiveOrigin',
+  'userSelect'
+]
+
+export const basePrefixes = [
+  '-webkit-',
+  '-moz-',
+  '-o-',
+  '-ms-',
+]
+
+export const prefixes = [
+  'Webkit',
+  'Moz',
+  'ms',
+]
